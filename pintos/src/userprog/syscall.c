f3a5003f1aa73481a6d14ec8d19404cdc2523491
DarkMarshal77 <tabanavid77@gmail.com>
2020-03-09 12:20:02 +0330

    write syscall

--- pintos/src/userprog/syscall.c
+++ pintos/src/userprog/syscall.c
@@ -9,27 +9,35 @@ static void syscall_handler (struct intr_frame *);
 void
 syscall_init (void)
 {
-  intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
+	intr_register_int (0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
 static void
 syscall_handler (struct intr_frame *f UNUSED)
 {
-  uint32_t* args = ((uint32_t*) f->esp);
+	uint32_t* args = ((uint32_t*) f->esp);
 
-  /*
-   * The following print statement, if uncommented, will print out the syscall
-   * number whenever a process enters a system call. You might find it useful
-   * when debugging. It will cause tests to fail, however, so you should not
-   * include it in your final submission.
-   */
+	/*
+	 * The following print statement, if uncommented, will print out the syscall
+	 * number whenever a process enters a system call. You might find it useful
+	 * when debugging. It will cause tests to fail, however, so you should not
+	 * include it in your final submission.
+	 */
 
-  /* printf("System call number: %d\n", args[0]); */
+	/* printf("System call number: %d\n", args[0]); */
 
-  if (args[0] == SYS_EXIT)
-    {
-      f->eax = args[1];
-      printf ("%s: exit(%d)\n", &thread_current ()->name, args[1]);
-      thread_exit ();
-    }
+	if (args[0] == SYS_EXIT)
+	{
+		f->eax = args[1];
+		printf ("%s: exit(%d)\n", &thread_current ()->name, args[1]);
+		thread_exit ();
+	}
+	else if (args[0] == SYS_WRITE)
+	{
+		int fd = args[1];
+		void *buf = args[2];
+		unsigned size = args[3];
+
+		f->eax = fwrite(buf, size, fd);
+	}
 }
